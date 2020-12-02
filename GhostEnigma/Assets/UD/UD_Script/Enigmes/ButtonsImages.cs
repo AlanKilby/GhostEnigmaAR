fcc9633bfd6a036c3cc30361f34cda2492928c1d
Ulric Debaert <ulric.debaert@outlook.fr>
2020-12-02 16:43:31 +0100

    EnigmeChimeraDébut

---
+++ GhostEnigma/Assets/UD/UD_Script/Enigmes/ButtonsImages.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsImages : MonoBehaviour
+{
+    [SerializeField] bool goodAnswer;
+    [SerializeField] bool wrongAnswer;
+
+    public static bool chimeraWinned;
+
+    private void Start()
+    {
+        chimeraWinned = false;
+    }
+
+    public void ButtonClicked()
+    {
+        if (goodAnswer)
+        {
+            chimeraWinned = true;
+        }
+        if (wrongAnswer)
+        {
+            chimeraWinned = false;
+            //Mettre le exit de l'interface
+        }
+    }
+}
