5ecfe9169d94f73438d0d40d92525295a4818598
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-12-02 15:05:32 +0100

    Project Push

---
+++ GhostEnigma/Assets/DistanceToObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceToObject : MonoBehaviour
+{
+    public ObjectSpawner objectSpawner;
+    public AudioClip[] beep;
+    public AudioSource audioBeep;
+    float distanceHolder;
+
+    private void Start()
+    {
+        distanceHolder = Vector3.Distance(transform.position, objectSpawner.objectList[0].transform.position)/2;
+    }
+
+    private void Update()
+    {
+        //if (Vector3.Distance(transform.position, objectSpawner.objectList[0].transform.position) > 2)
+        //{
+        //    if(audioBeep.clip != beep[0])
+        //    {
+        //        audioBeep.clip = beep[0];
+        //        audioBeep.Play();
+        //    }
+        //}
+        //else if(Vector3.Distance(transform.position,objectSpawner.objectList[0].transform.position) < 2 &&  (Vector3.Distance(transform.position, objectSpawner.objectList[0].transform.position) > 0.5))
+        //{
+        //    if (audioBeep.clip != beep[1])
+        //    {
+        //        audioBeep.clip = beep[1];
+        //        audioBeep.Play();
+        //    }
+            
+        //}
+        //else if (Vector3.Distance(transform.position, objectSpawner.objectList[0].transform.position) < 0.5)
+        //{
+        //    if (audioBeep.clip != beep[2])
+        //    {
+        //        audioBeep.clip = beep[2];
+        //        audioBeep.Play();
+        //    }
+        //}
+
+        if(distanceHolder >= 0)
+        {
+            distanceHolder -= Time.deltaTime;
+        }
+
+        if(distanceHolder <= 0)
+        {
+            audioBeep.clip = beep[0];
+            audioBeep.Play();
+            distanceHolder = Vector3.Distance(transform.position, objectSpawner.objectList[0].transform.position)/2;
+        }
+        
+
+    }
+}
---
+++ GhostEnigma/Assets/ObjectSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public GameObject[] objectList;
+    public float spaceSize;
+
+
+    private void Start()
+    {
+        
+        
+        objectList[0] = Instantiate(objectToSpawn, new Vector3(Random.Range(-spaceSize, spaceSize), -0.7f, Random.Range(-spaceSize, spaceSize)), Quaternion.identity);
+        
+    }
+
+    private void Update()
+    {
+        //if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        //{
+        //    GameObject obj = Instantiate(objectToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+        //}
+
+
+    }
+
+    public void GenerateNewObject()
+    {
+        objectList[0] = Instantiate(objectToSpawn, new Vector3(Random.Range(-spaceSize, spaceSize), -0.7f, Random.Range(-spaceSize, spaceSize)), Quaternion.identity);
+    }
+}
