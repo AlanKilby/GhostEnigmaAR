4053430b90723a726656a6f53e6a2876485ca984
Djason Nathiez <nathiez.djason@outlook.fr>
2020-12-03 16:24:54 +0100

    UI without script connection

---
+++ GhostEnigma/Assets/DN/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class UIManager : MonoBehaviour
+{
+    public Slider detector;
+    public TextMeshProUGUI enigmaCounter;
+    PlayerInteractions player;
+
+    [Range(0, 1)]
+    public float detectorValue;
+    public float enigmaValue;
+
+    private void Start()
+    {
+      
+    }
+    private void Update()
+    {
+        if (player)
+        {
+            enigmaValue = player.enigmaValidator;
+        }
+        detector.value = detectorValue;
+        enigmaCounter.text = enigmaValue.ToString() + "/3";
+    }
+}
